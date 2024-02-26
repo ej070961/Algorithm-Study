ef2e05313a4dcf3152293919107b735b693f1f26
changha <mentenseoul@gmail.com>
2024-02-26 16:50:31 +0900

    create BOJ-1929

--- BOJ/수학/1929 소수구하기/changha.py
+++ BOJ/수학/1929 소수구하기/changha.py
@@ -1,1 +1,20 @@
-#start
+m, n = map(int, input().split())
+arr = []
+def is_prime(x):
+    # 1에 대한 처리 
+    if x == 1:
+        return False
+
+    for i in range(2, x):
+        #나뉜다면 False 반환 
+        if x % i == 0:
+            return False
+    # 그외에는 True 
+    return True
+
+for i in range(m, n+1):
+    if is_prime(i):
+        arr.append(i)
+
+for v in arr:
+    print(v)
