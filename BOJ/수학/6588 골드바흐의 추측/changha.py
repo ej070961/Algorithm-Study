1246c6c7ce1c538c43e8a27ff18e4a5db9358701
changha <mentenseoul@gmail.com>
2024-02-29 15:29:19 +0900

    update BOJ-6588-골드바흐의 추측

--- BOJ/수학/6588 골드바흐의 추측/changha.py
+++ BOJ/수학/6588 골드바흐의 추측/changha.py
@@ -1,23 +1,28 @@
 import math
+import sys 
 
-def is_prime(x):
-    for i in range(2, int(math.sqrt(x))):
-        if x % i == 0:
-            return False
-    return True
+# 에라토스테네스의 체 == 주어진 범위 내의 소수 리스트 반환하는 효율적인 알고리즘 
+def sieve_of_eratosthenes(n):
+    prime = [True for _ in range(n+1)]
+    prime[0] = prime[1] = False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if prime[i] == True:
+            for j in range(i*i, n+1, i):
+                prime[j] = False
+    return prime
+
+max_n = 1000000
+prime_list = sieve_of_eratosthenes(max_n)
 
 while True:
-    n = int(input())
+    n = int(sys.stdin.readline().strip())
     if n == 0:
         break
-    
-    a_prime = []
-    for i in range(3, n//2 + 1):
-        if is_prime(i):
-            a_prime.append(i)
-        
-    for a in a_prime:
-        b = n - a
-        if is_prime(b):
-            print(str(n) + " = " + str(a) + " + " + str(b))
+
+    for i in range(3, int(n/2)+1, 2):
+        if prime_list[i] and prime_list[n-i]:
+            print(f"{n} = {i} + {n-i}")
             break
+
+    else:
+        print("Goldbach's conjecture is wrong.")
