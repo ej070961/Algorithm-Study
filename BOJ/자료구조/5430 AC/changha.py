caee7706505feb15e285a3f77a76c82e3434115b
changha <mentenseoul@gmail.com>
2024-03-08 16:51:36 +0900

    update BOJ-5430-AC

--- BOJ/자료구조/5430 AC/changha.py
+++ BOJ/자료구조/5430 AC/changha.py
@@ -7,39 +7,35 @@ import sys
 from collections import deque
 t = int(sys.stdin.readline())
 
-def reverse_arr(arr):
-    arr.reverse()
-def delete_arr(arr):
-    if len(arr) != 0:
-        arr.popleft()
-        return False
-    else:
-        return True
     
 for _ in range(t):
     fun_arr = list(map(str, sys.stdin.readline()))
     n = int(sys.stdin.readline())
-    in_arr = sys.stdin.readline().strip()
-    # 문자열에서 숫자만 추출하기 위해 리스트 표현의 괄호를 제거하고, 쉼표로 구분된 숫자를 분리
-    numbers_str = in_arr.strip('[]').split(',')
-    # numbers_str = ['1', '2', '3']
+    numbers = sys.stdin.readline().rstrip()[1:-1].split(",")
 
-    # 문자열 리스트를 정수 리스트로 변환
+    queue = deque(numbers)
+    r = 0
+
     if n == 0:
-        print("error")
-        continue
-    numbers = deque(list(map(int, numbers_str)))
+        queue = []
+        
     
-    error = False
     for i in fun_arr:
         if i == "R":
-            reverse_arr(numbers)
+            r += 1
         elif i == "D":
-            error = delete_arr(numbers)
-            if error:
+            if len(queue) == 0:
+                print("error")
                 break
-
-    if error:
-        print("error")
+            else:
+                if r % 2 == 0:
+                    queue.popleft()
+                else:
+                    queue.pop()
     else:
-        print(list(numbers))
+        if r % 2 == 0:
+            print('[' + ','.join(queue) + ']')
+        else:
+            queue.reverse()
+            print('[' + ','.join(queue) + ']')
+
