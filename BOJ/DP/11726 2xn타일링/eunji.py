adebe4ba177a2191530e9cfdee1ce24a1580fe77
ej070961 <ej070961@gmail.com>
2024-03-19 14:58:32 +0900

    update BOJ-11726-2xn타일링

--- BOJ/DP/11726 2xn타일링/eunji.py
+++ BOJ/DP/11726 2xn타일링/eunji.py
@@ -6,14 +6,20 @@ n= int(sys.stdin.readline())
 #2*n 직사각형 채우기
 dp = [0]*(n+1)
 
-#n이 1일 때
-dp[1]=1
-#n이 2일 때 
-dp[2]=2
+if n==1:
+    print(1)
+elif n==2:
+    print(2)
+else: 
+    #n이 1일 때
+    dp[1]=1
+    #n이 2일 때 
 
-for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+    dp[2]=2
+
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
         
 
-print(dp[n]%10007)
+    print(dp[n]%10007)
 
