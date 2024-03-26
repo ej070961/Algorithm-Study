35449913332844822d258c3f7c8cbec495d14d51
changha <mentenseoul@gmail.com>
2024-03-26 15:48:03 +0900

    create BOJ-1012-유기농배추

--- BOJ/DP/1912 연속합/changha.py
+++ BOJ/DP/1912 연속합/changha.py
@@ -2,7 +2,18 @@ n = int(input())
 
 input_list = list(map(int, input().split()))
 
-for i in range(1, n):
-    input_list[i] = max(input_list[i-1] + input_list[i], input_list[i])
+# for i in range(1, n):
+#     input_list[i] = max(input_list[i-1] + input_list[i], input_list[i])
     
-print(max(input_list))
+# print(max(input_list))
+
+total_max = input_list[0]
+cur_max = [input_list[0]] * n
+
+for i in range(1, n):
+    cur_max[i] = max(input_list[i] + cur_max[i-1], input_list[i])
+    total_max = max(total_max, cur_max[i])
+
+print(total_max)
+
+
